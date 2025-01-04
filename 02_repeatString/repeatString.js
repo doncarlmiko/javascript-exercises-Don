e22c787c154be0fd437d9a725ed2fd63d1906584
doncarlmiko <konoha11061997@gmail.com>
2025-01-04 22:03:28 +0800

    passed all the test in repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let stringSpace = '';
+    let numStart;
+    
+    if(num<0) return "ERROR";
+    for(numStart = 0; numStart < num; numStart++){
+        stringSpace += string;
+    }
+    return stringSpace;
 };
 
 // Do not edit below this line
