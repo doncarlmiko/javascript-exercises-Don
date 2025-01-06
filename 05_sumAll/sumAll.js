10349ceb90e1501b98a7b7f6dd534dc32b3d355f
doncarlmiko <konoha11061997@gmail.com>
2025-01-06 23:08:17 +0800

    Passed all the test required for sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
 
+    if(num1 <0 || num2 <0){
+        return 'ERROR';
+    }
+
+    if(typeof num1 !=='number' || typeof num2 !== 'number' || Array.isArray(num1) || Array.isArray(num1)){
+        return "ERROR";
+    }
+
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
+
+    let getMin = num1 < num2 ? num1 : num2;
+    let getMax = num1 > num2 ? num1 : num2;
+    let totalSum = 0;
+
+    for(let i = getMin; i <=getMax; i++){
+        totalSum += i;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
