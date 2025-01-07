8d782fb8a7e4b6e2985267b83f1b21233d791b98
doncarlmiko <konoha11061997@gmail.com>
2025-01-07 22:39:36 +0800

    passed all tests for exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitValue) {
+  //Formula for converting Fahrenheit to Celsius
+  const convertToCelsius = (fahrenheitValue-32)/1.8;
+  let roundConvertToCelsius = Math.round(convertToCelsius*10)/10;
+
+  return roundConvertToCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusValue) {
+  //Formula for converting Celsius to Fahrenheit
+  const convertToFahrenheit = (celciusValue * 1.8) + 32;
+  let roundConvertToFahrenheit = Math.round(convertToFahrenheit*10)/10;
+
+  return roundConvertToFahrenheit;
+
 };
 
 // Do not edit below this line
