5803ff659e88b1f12de5f8fc9138448b43ee0514
doncarlmiko <konoha11061997@gmail.com>
2025-01-12 16:41:12 +0800

    Passed all test on the palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (palindromeString) {
+    const normalizeString = palindromeString.toLowerCase().replace(/[^a-z0-9]/g,"");
+    let reverseString = normalizeString.split("").reverse().join("");
 
+    if (normalizeString === reverseString) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
