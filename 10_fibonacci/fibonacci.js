91fa15055cab108835fc2620c4b04187b0b819ed
doncarlmiko <konoha11061997@gmail.com>
2025-01-19 15:47:31 +0800

    Updated the psuedocode and passed the three test runs

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciSequenceposition) {
+    const fibonacciSequenceArray = [1,1];
+    let fibonacciLoopCounter = 0;
+    let fibonacciSum;
+    for(fibonacciLoopCounter =1; fibonacciLoopCounter <fibonacciSequenceposition; fibonacciLoopCounter++){
+        
+            fibonacciSum = fibonacciSequenceArray[fibonacciSequenceArray.length-1] + fibonacciSequenceArray[fibonacciLoopCounter-1];
+
+            fibonacciSequenceArray.push(fibonacciSum);
+    }
+    return fibonacciSequenceArray[fibonacciSequenceposition-1];
 
 };
 
