256bcd2e247223d09b4846857917adc06392949b
doncarlmiko <konoha11061997@gmail.com>
2025-01-19 16:39:24 +0800

    Passed all tests for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,15 +1,30 @@
 const fibonacci = function(fibonacciSequenceposition) {
+
+    let fibonacciSequenceNum = Number(fibonacciSequenceposition);
     const fibonacciSequenceArray = [1,1];
     let fibonacciLoopCounter = 0;
     let fibonacciSum;
-    for(fibonacciLoopCounter =1; fibonacciLoopCounter <fibonacciSequenceposition; fibonacciLoopCounter++){
-        
+
+    for(fibonacciLoopCounter = 1; fibonacciLoopCounter <fibonacciSequenceNum; fibonacciLoopCounter++){
+
+        if(fibonacciLoopCounter>=1){
             fibonacciSum = fibonacciSequenceArray[fibonacciSequenceArray.length-1] + fibonacciSequenceArray[fibonacciLoopCounter-1];
 
             fibonacciSequenceArray.push(fibonacciSum);
+        }
+        
     }
-    return fibonacciSequenceArray[fibonacciSequenceposition-1];
+    
+    if(fibonacciSequenceNum === 0){
+        return 0;
+    }
+    else if(fibonacciSequenceNum < 0){
+        return "OOPS";
+    }
 
+    else{
+        return fibonacciSequenceArray[fibonacciSequenceNum-1];
+    }
 };
 
 // Do not edit below this line
