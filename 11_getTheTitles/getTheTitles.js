5899796f512338b00b414360d21e88ea976f59ba
doncarlmiko <konoha11061997@gmail.com>
2025-01-20 19:56:51 +0800

    Passed all tests on the 11th exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let bookTitlesCounter;
+    const bookTitleArray = [];
 
+      for(bookTitlesCounter = 0; bookTitlesCounter <=books.length-1; bookTitlesCounter++){
+        
+        bookTitleArray.push(books[bookTitlesCounter].title);
+    }
+    return bookTitleArray;
 };
 
 // Do not edit below this line
